9efec3a6cc34bc2f29c75cf25c4bd96e79b090a5
TranHang12 <tranhangsocute12@gmail.com>
2022-09-29 17:25:23 +0700

    update filter

--- src/components/FilteringTable.js
+++ src/components/FilteringTable.js
@@ -1,6 +1,8 @@
 import React, { useMemo } from 'react'
 import { useTable } from 'react-table'
 import { COLUMNS } from './columns'
+import {MOCK_DATA} from './MOCK_DATA.json'
+import './table.css'
 
 export const FilteringTable = () => {
     const columns = useMemo(() => COLUMNS, [])
@@ -21,6 +23,33 @@ export const FilteringTable = () => {
     )
     
   return (
-    <div>FilteringTable</div>
+    <table {...getTableProps()}>
+      <thead>
+        {
+          headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {
+                headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render('Header')}
+                  </th>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </thead>
+
+      <tbody {...getTableBodyProps()}>
+        {
+          rows.map((row) => {
+            prepareRow(row)
+            return (
+              <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+            )
+          })
+        }
+      </tbody>
+    </table>
   )
 }
